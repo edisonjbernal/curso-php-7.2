98cd034826b99de5251c1ffc85a9db049df13761
Edison Johan Bernal <edison@mejorvirtual.com>
2018-11-28 10:19:41 -0500

    Ver diferencias entre HEREDOC y NOWDOC

--- index.php
+++ index.php
@@ -1,3 +1,29 @@
+<?php
+
+//Si se utilizan comillas dobles el sistema va intentar mostrar también el valor de las variables.
+$lastName='Bernal Muñoz';
+//$name='Edison Johan';
+//var_dump($name); //Ver el contenido del dato. string(12) "Edison Johan"
+//HEARDOC ->
+
+//HEREDOC Similar a comillas sobles,
+$name=<<<EOT
+Edison
+Johan $lastName
+EOT;
+// O
+
+$name=<<<"EOT"
+Edison
+Johan
+EOT;
+
+//NOWDOC Similar a comillas sencillas
+$name=<<<'EOT'
+Edison
+Johan $lastName
+EOT;
+?>
 <!doctype html>
 <html lang="es">
 
@@ -21,7 +47,7 @@
         <img id="profile-picture" src="https://ui-avatars.com/api/?name=John+Doe&size=255" alt="">
       </div>
       <div class="col">
-        <h1><?php echo 'Edison Johan Bernal Muñoz'; ?></h1>
+        <h1><?php echo $name.' '.$lastName; ?></h1>
         <h2>PHP Developer</h2>
         <ul>
           <li>Mail: hector@mail.com</li>
