6811403906e9d9373f7711ba59bcdd6fe8d756c8
Edison Bernal <edison@mejorvirtual.com>
2018-12-07 11:22:22 -0500

    Front Controller
    Front Controller es un patrón de diseño que nos dará una solución a la redundancia del código en las múltiples entradas a la aplicación. Este tendrá el control de todas las entradas además de las inicializaciones de base de datos etc.

--- index.php
+++ index.php
@@ -1,30 +1,5 @@
 <?php
-require_once 'vendor/autoload.php';
-
-use Illuminate\Database\Capsule\Manager as Capsule;
 use App\Models\Job;
-
-$capsule = new Capsule;
-
-$capsule->addConnection([
-    'driver'    => 'mysql',
-    'host'      => 'localhost',
-    'database'  => 'cursophp',
-    'username'  => 'platzi',
-    'password'  => 'platziCourse23#',
-    'charset'   => 'utf8',
-    'collation' => 'utf8_unicode_ci',
-    'prefix'    => '',
-]);
-
-// Make this Capsule instance available globally via static methods... (optional)
-//Permite hacer todo como si estuviera en el contexto global
-$capsule->setAsGlobal();
-
-// Setup the Eloquent ORM... (optional; unless you've used setEventDispatcher())
-//Inicializar el ORM
-$capsule->bootEloquent();
-
 //Si se utilizan comillas dobles el sistema va intentar mostrar también el valor de las variables.
 require_once('jobs.php');
 
