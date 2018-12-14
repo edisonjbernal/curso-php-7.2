9fb4b3ff491f163d8477bc344e751378e14c4ea1
Edison Bernal <edison@mejorvirtual.com>
2018-12-14 07:53:50 -0500

    Extendiendo Templates con Twig
    Extender templates nos permitirá hacer sitios más dinámicos reutilizando código HTML.
    
    Denotaremos con la palabra reservada extends el código que queremos reutilizar y con la palabra block el código que deseamos sea dinámico, este último debe tener una palabra clave que le sirva de identificador para el código que se estará reemplazando.

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -5,7 +5,7 @@ namespace App\Controllers;
 use Zend\Diactoros\Response\HtmlResponse;
 
 class BaseController{
-  protected $templateEngine;
+  protected $templateEngine = '';
 
   public function __construct(){
 
@@ -13,13 +13,13 @@ class BaseController{
       //use \Twig_Loader_Filesystem
       $loader = new \Twig_Loader_Filesystem('../views');
 
-      $this->$templateEngine = new \Twig_Environment($loader, array(
+      $this->templateEngine = new \Twig_Environment($loader, array(
     'debug' => true,
     'cache' => false,
 ));
   }
 
   public function renderHTML($fileName,$data = []){
-    return new HtmlResponse($this->$templateEngine->render($fileName,$data));
+    return new HtmlResponse($this->templateEngine->render($fileName,$data));
   }
 }
--- app/Controllers/JobsController.php
+++ app/Controllers/JobsController.php
@@ -24,7 +24,7 @@ class JobsController extends BaseController{
       $job->description = $postData['description'];
       $job->save();
     }
-    echo $this->renderHTML('addJob.twig');
+    return $this->renderHTML('addJob.twig');
   }
 
 
